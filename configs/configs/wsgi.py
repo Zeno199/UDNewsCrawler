72d141c68e5934b0b5e9e2171641e6194c0ca830
Zeno <fgis2gi@gmail.com>
2019-08-22 23:04:50 +0800

    my djangogirls app

--- configs/configs/wsgi.py
+++ configs/configs/wsgi.py
@@ -12,7 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 from dj_static import Cling
-from whitenoise.django import DjangoWhiteNoise
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configs.settings')
